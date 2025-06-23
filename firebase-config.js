eadd08bf3ef267d7c1fb45b38423c8d75b84a2a3
raspberriestree <denizemanuel2003@gmail.com>
2025-06-23 13:51:41 -0300

    firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,12 @@
 // firebase-config.js
 const firebaseConfig = {
-  apiKey: "AIzaSyD...",
-  authDomain: "tu-proyecto.firebaseapp.com",
-  projectId: "tu-proyecto",
-  storageBucket: "tu-proyecto.appspot.com",
-  messagingSenderId: "123456789",
-  appId: "1:123456789:web:abc123def456"
+  apiKey: "AIzaSyD1DWHev_R6P24dCsSMQdkeK-Mm7OoBSGM",
+  authDomain: "military-squad-stats.firebaseapp.com",
+  projectId: "military-squad-stats",
+  storageBucket: "military-squad-stats.firebasestorage.app",
+  messagingSenderId: "1016112102578",
+  appId: "1:1016112102578:web:52f4046e47a12da639ddab",
+  measurementId: "G-H5X1QEHXFV"
 };
+
+export { firebaseConfig };
