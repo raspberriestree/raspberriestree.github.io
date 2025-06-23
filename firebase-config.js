fecabbfd8bc1322005b3cce88b6817949ab76775
raspberriestree <denizemanuel2003@gmail.com>
2025-06-23 13:34:14 -0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+// firebase-config.js
+const firebaseConfig = {
+  apiKey: "AIzaSyD...",
+  authDomain: "tu-proyecto.firebaseapp.com",
+  projectId: "tu-proyecto",
+  storageBucket: "tu-proyecto.appspot.com",
+  messagingSenderId: "123456789",
+  appId: "1:123456789:web:abc123def456"
+};
